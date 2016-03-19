af10ae22cf5838a988b6a2760ec1835631ebc3b6
Ravi <ravi.woods@gmail.com>
2016-03-19 15:15:47 +0000

    "BothTimeouts"

--- DatagramServer.java
+++ DatagramServer.java
@@ -11,7 +11,7 @@ import java.net.* ;
  
 public class DatagramServer
 {
-   private final static int PACKETSIZE = 100 ;
+   private final static int PACKETSIZE = 200000 ;
 
    public static void main( String args[] )
    {

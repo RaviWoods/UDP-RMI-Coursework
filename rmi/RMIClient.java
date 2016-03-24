22631ead77b0c9875699e14aa04572be8667be57
Ravi <ravi.woods@gmail.com>
2016-03-24 20:50:18 +0000

    "Edits4"

--- rmi/RMIClient.java
+++ rmi/RMIClient.java
@@ -20,6 +20,7 @@ public class RMIClient {
 
 		RMIServerI iRMIServer = null;
 
+		// Pass server IPAdress and number of messages to send in the command line
 		// Check arguments for Server host and number of messages
 		if (args.length < 2){
 			System.out.println("Needs 2 arguments: ServerHostName/IPAddress, TotalMessageCount");
@@ -28,12 +29,27 @@ public class RMIClient {
 
 		String urlServer = new String("rmi://" + args[0] + "/RMIServer");
 		int numMessages = Integer.parseInt(args[1]);
+ 
+ 		try{
+			// TO-DO: Initialise Security Manager
+			if (System.getSecurityManager() == null){
+				System.setSecurityManager(new RMISecurityManager());
+			}
 
-		// TO-DO: Initialise Security Manager
+			// TO-DO: Bind to RMIServer
+			try{
+				iRMIServer = (RMIServerI) Naming.lookup(urlServer);
+			} catch (MalformedURLException e){	
+				e.printStackTrace();
+			} catch (NotBoundException e){
+				e.printStackTrace();
+			}
 
-		// TO-DO: Bind to RMIServer
-
-		// TO-DO: Attempt to send messages the specified number of times
-
+			// TO-DO: Attempt to send messages the specified number of times
+			for(int n = 0; n < numMessages; n++)
+				iRMIServer.receiveMessage(new MessageInfo(numMessages,n));
+		} catch (RemoteException e){
+			System.out.println("Remote Exception: " + e);
+		}
 	}
 }
